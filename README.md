3a06a6eb4c75e306154e06e7b61651008be9d2bf
tianzhongbo <tianzhongbo@douban.com>
2014-10-09 12:44:50 +0800

    prepare for pypi

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 A pure python implementation of Mesos scheduler and executor
-
-Forked from https://github.com/douban/dpark/tree/master/dpark/pymesos
--- setup.py
+++ setup.py
@@ -3,6 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name='pymesos',
     version='0.0.1',
+    description="A pure python implementation of Mesos scheduler and executor",
     packages=find_packages(),
     install_requires=['mesos.interface'],
+    platforms=['POSIX'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+    ],
+    author="Zhongbo Tian",
+    author_email="tianzhongbo@douban.com",
+    url="https://github.com/douban/pymesos",
+    download_url = 'https://github.com/douban/pymesos/archive/0.0.1.tar.gz',
 )
