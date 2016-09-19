86dd4eed0e7bdec48470af7c0d85082d6f978971
tianzhongbo <tianzhongbo@douban.com>
2016-09-19 12:29:20 +0800

    add travis

--- pymesos/__init__.py
+++ pymesos/__init__.py
@@ -1,8 +1,8 @@
-__VERSION__ = '0.2.0'
-
 from .interface import Scheduler, Executor
 from .scheduler import MesosSchedulerDriver
 from .executor import MesosExecutorDriver
+
+__VERSION__ = '0.2.0'
 
 __all__ = (
     'Scheduler',
