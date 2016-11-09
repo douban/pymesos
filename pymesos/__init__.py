acff08447d8b251879c0126e9a8b29c8bfd9158c
windreamer <windreamer@gmail.com>
2016-11-09 10:28:04 +0800

    Update __init__.py

--- pymesos/__init__.py
+++ pymesos/__init__.py
@@ -3,7 +3,7 @@ from .scheduler import MesosSchedulerDriver
 from .executor import MesosExecutorDriver
 from .utils import encode_data, decode_data
 
-__VERSION__ = '0.2.5'
+__VERSION__ = '0.2.6'
 
 __all__ = (
     'Scheduler',
