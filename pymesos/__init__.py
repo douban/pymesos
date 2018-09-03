bf4ee5d8afbc048d863b5aa5cd5e302a95ae7ae3
ZHAO Yijun <recursion@null.net>
2018-09-03 15:34:36 +0800

    add logger handler

--- pymesos/__init__.py
+++ pymesos/__init__.py
@@ -1,16 +1,25 @@
+import sys
+import logging
+
 from .interface import Scheduler, Executor, OperatorMaster
 from .scheduler import MesosSchedulerDriver
 from .executor import MesosExecutorDriver
 from .operator_v1 import MesosOperatorMasterDriver, MesosOperatorAgentDriver
 from .utils import encode_data, decode_data
-import logging
 
 
-fmt = '%(asctime)-15s [%(levelname)s] [%(threadName)s] [%(name)-9s:%(lineno)d] %(message)s'
-logging.basicConfig(format=fmt, level=logging.INFO)
+LOG_FMT = '%(asctime)-15s [%(levelname)s] [%(threadName)s]' \
+      ' [%(name)-9s:%(lineno)d] %(message)s'
+DATE_FMT = '%Y-%m-%d %H:%M:%S'
+
+logger = logging.getLogger('pymesos')
+handler = logging.StreamHandler(stream=sys.stderr)
+handler.setFormatter(logging.Formatter(LOG_FMT, DATE_FMT))
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 
-__VERSION__ = '0.3.8'
+__VERSION__ = '0.3.9'
 
 __all__ = (
     'Scheduler',
